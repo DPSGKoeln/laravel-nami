ac5c544c32b21f5f6b0c281b216baa13ab2feda8
philipp lang <philipp@aweos.de>
2020-04-10 20:42:57 +0200

    Initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zoomyboy\LaravelNamiGuard;
+
+use Illuminate\Support\ServiceProvider;
+use Riak\Connection;
+
+class NamiGuardServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+}
